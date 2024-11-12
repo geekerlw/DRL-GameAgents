4a65b16dcc37045f1b2437a1b39a25f3e2dc4d74
Steven Lee <geekerlw@gmail.com>
2024-11-12 15:41:41 +0800

    add simple train and evaluate

---
+++ main.py
@@ -0,0 +1,12 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from env import RBREnv
+
+def evaluate():
+    env = RBREnv(shakedown=False)
+    model = PPO.load("model/ppo-rbragent", env=env)
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+    print("final evaluate result: {mean_reward}, {std_reward}")
+
+if __name__ == '__main__':
+    evaluate()
--- train.py
+++ train.py
@@ -1,16 +1,26 @@
+import os
 from stable_baselines3 import PPO
 from env import RBREnv
 
+MODEL_DIR = "model"
+HALF_MODEL = "ppo-rbragent-half"
+FINAL_MODEL = "ppo-rbragent"
+
 def train():
+    os.makedirs(MODEL_DIR, exist_ok=True)
     try:
         env = RBREnv(shakedown=True)
-        model = PPO("MlpPolicy", env, verbose=1)
-        model.learn(total_timesteps=10_000)
+        if os.path.exists(os.path.join(MODEL_DIR, HALF_MODEL) + ".zip"):
+            model = PPO.load(os.path.join(MODEL_DIR, HALF_MODEL), env=env)
+        else:
+            model = PPO("MlpPolicy", env, verbose=1)
+        model.learn(total_timesteps=10_000, progress_bar=True)
     except Exception as e:
-        print("training abort: {e}")
-        model.save("part_model")
+        print("training abort: {e}, save half trained model.")
+        model.save(os.path.join(MODEL_DIR, HALF_MODEL))
 
-    model.save("final_model")
+    print("training finished, save final model.")
+    model.save(os.path.join(MODEL_DIR, FINAL_MODEL))
 
 if __name__ == '__main__':
     train()
