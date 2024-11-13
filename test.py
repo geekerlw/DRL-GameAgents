4be664fd513a29bcbfc0219b7661ee15a5e700e0
Steven Lee <geelerlw@gmail.com>
2024-11-13 20:44:05 +0800

    fix test code

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from stable_baselines3 import PPO
 from env import RBREnv
 
-def train_no_save():
+def train():
     env = RBREnv(shakedown=True)
     model = PPO("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=10_000)
