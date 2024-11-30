f8d346f79a72e6a0c18cf72aa710f55b9a7569f2
Steven Lee <geelerlw@gmail.com>
2024-11-30 17:08:44 +0800

    add a game monitor to support simple CNN

--- track.py
+++ track.py
@@ -1,76 +1,96 @@
 import pygame
 import numpy as np
 import utils
+import ctypes
+import threading
 from driveline import DriveLine
+from game import RBRGame
 
-# 初始化 Pygame
-pygame.init()
-
-# 设置窗口大小
-screen_width, screen_height = 512, 512
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-# 设置颜色
 TRACK_COLOR = (50, 150, 50)  # 赛道颜色
 BOUNDARY_COLOR = (0, 0, 0)   # 赛道边界颜色
-WHITE = (255, 255, 255)
-SCALE = 5
+WHITE = (255, 255, 255) # 赛道背景颜色
+SCREEN_SIZE = (480, 480)
 
-# 定义车辆的初始位置（底部居中）
-car_x, car_y = screen_width // 2, screen_height - 30  # 底部居中
-car_pos = (103, -61)
+class TrackMonitor(threading.Thread):
+    def __init__(self, game: RBRGame, driveline: DriveLine):
+        super().__init__()
+        self.game = game
+        self.driveline = driveline
+        self.running = True
+        self.fixed_pos = (SCREEN_SIZE[0] // 2, SCREEN_SIZE[1]-20)
+        self.update_region = True
+        self.start()
 
-driveline = DriveLine()
-driveline.load(318)
+    def stop(self):
+        self.running = False
 
-# select nearby points
-left, right, center = driveline.nearby(50)
-left = [(point[0] * SCALE, point[1] * SCALE) for point in left]
-right = [(point[0] * SCALE, point[1] * SCALE) for point in right]
+    def run(self):
+        pygame.init()
+        screen = pygame.display.set_mode(SCREEN_SIZE, pygame.NOFRAME)
+        hwnd = pygame.display.get_wm_info()["window"]
+        ctypes.windll.user32.SetWindowPos(hwnd, None, 650, 0, SCREEN_SIZE[0], SCREEN_SIZE[1], 0)
 
-# rotate and translation
-rotation = np.degrees(np.arctan2(1, 0) - np.arctan2(center[0][4], center[0][3]))
-translation = (car_x - car_pos[0], car_y - car_pos[1])
-matrix = utils.create_transform_matrix(rotation, translation)
-print(translation)
-print(matrix)
-left = utils.calculate_transform_points(left, matrix)
-right = utils.calculate_transform_points(right, matrix)
-car = utils.calculate_transform_point(car_pos, matrix)
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-print(car)
+            left, right= self.update_track()
+            car = self.update_car()
 
-# convert to pygame's coordinate
-track_left = [(point[0], point[1]) for point in left]
-track_right = [(point[0], point[1]) for point in right]
+            # 填充背景
+            screen.fill(WHITE)
 
-print(track_left)
+            # 绘制赛道中间部分
+            pygame.draw.polygon(screen, TRACK_COLOR, left + right[::-1])
 
-# track_left = [(car_x - 25, screen_height), (car_x - 50, 300), (car_x + 50, 200), (car_x - 25, 100)]  # 左边界
-# track_right = [(car_x + 25, screen_height), (car_x + 0, 300), (car_x + 100, 200), (car_x + 25, 100)]  # 右边界
+            # 绘制赛道边界
+            pygame.draw.lines(screen, BOUNDARY_COLOR, False, left, 5)
+            pygame.draw.lines(screen, BOUNDARY_COLOR, False, right, 5)
 
-# 主循环
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+            # 绘制车辆（简单地用一个矩形表示，车辆中心位置）
+            pygame.draw.rect(screen, (255, 0, 0), (car[0] - 6, car[1] - 4, 12, 8))
 
-    # 填充背景
-    screen.fill(WHITE)
+            # 更新显示
+            pygame.display.flip()
+            pygame.time.Clock().tick(30)
 
-    # 绘制赛道中间部分
-    pygame.draw.polygon(screen, TRACK_COLOR, track_left + track_right[::-1])
+        pygame.quit()
 
-    # 绘制赛道边界
-    pygame.draw.lines(screen, BOUNDARY_COLOR, False, track_left, 5)
-    pygame.draw.lines(screen, BOUNDARY_COLOR, False, track_right, 5)
+    def update_track(self):
+        if self.update_region:
+            drive_distance = self.game.drive_distance()
+            left, right, center = self.driveline.nearby(drive_distance, 10, 120)
+            start = (center[0][0], center[0][1])
+            direction = (center[0][3], center[0][4])
 
-    # 绘制车辆（简单地用一个矩形表示，车辆中心位置）
-    pygame.draw.rect(screen, (255, 0, 0), (car_x - 15, screen_height - 50, 30, 20))
+            rotation = np.degrees(np.arctan2(-1, 0) - np.arctan2(direction[1], direction[0]))
+            translation = (self.fixed_pos[0] - start[0], self.fixed_pos[1] - start[1])
+            matrix = utils.create_transform_matrix(start, rotation, translation, scale=5.0)
+            self.left = utils.calculate_transform_points(left, matrix)
+            self.right = utils.calculate_transform_points(right, matrix)
+            self.matrix = matrix
+            self.update_region = False
 
-    # 更新显示
-    pygame.display.flip()
+        return self.left, self.right
 
-# 退出 Pygame
-pygame.quit()
+    def update_car(self):
+        car_pos = self.game.car_pos()[0:2]
+        car = utils.calculate_transform_point(car_pos, self.matrix)
+
+        (x, y) = car
+
+        if x < 0 or x > SCREEN_SIZE[0] or y < 0 or y > SCREEN_SIZE[1]:
+            self.update_region = True
+
+        return car
+    
+
+if __name__ == '__main__':
+    rbr = RBRGame()
+    driveline = DriveLine()
+    rbr.attach()
+    driveline.load(318)
+
+    track = TrackMonitor(rbr, driveline)
+    track.join()
